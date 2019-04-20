f1950a5bd7a8a29a1db0270d2f74fcad18e3ec04
Karthik Mukkala <karthik.mukkala@milvik.se>
2019-04-21 01:28:42 +0530

    Implementing pet clinic POJO data model

---
+++ src/main/java/com/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
